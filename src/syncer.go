e919a4b418b362eafb071b3f99dce6492fcacb56
exAspArk <exaspark@gmail.com>
2025-03-10 11:53:26 -0400

    Extract SyncerFullRefresh

--- src/syncer.go
+++ src/syncer.go
@@ -3,16 +3,12 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"runtime"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -25,9 +21,10 @@ const (
 )
 
 type Syncer struct {
-	config        *Config
-	icebergWriter *IcebergWriter
-	icebergReader *IcebergReader
+	config            *Config
+	icebergWriter     *IcebergWriter
+	icebergReader     *IcebergReader
+	syncerFullRefresh *SyncerFullRefresh
 }
 
 func NewSyncer(config *Config) *Syncer {
@@ -37,7 +34,12 @@ func NewSyncer(config *Config) *Syncer {
 
 	icebergWriter := NewIcebergWriter(config)
 	icebergReader := NewIcebergReader(config)
-	return &Syncer{config: config, icebergWriter: icebergWriter, icebergReader: icebergReader}
+	return &Syncer{
+		config:            config,
+		icebergWriter:     icebergWriter,
+		icebergReader:     icebergReader,
+		syncerFullRefresh: NewSyncerFullRefresh(config),
+	}
 }
 
 func (syncer *Syncer) SyncFromPostgres() {
@@ -56,7 +58,7 @@ func (syncer *Syncer) SyncFromPostgres() {
 		for _, pgSchemaTable := range syncer.listPgSchemaTables(structureConn, schema) {
 			if syncer.shouldSyncTable(pgSchemaTable) {
 				pgSchemaTables = append(pgSchemaTables, pgSchemaTable)
-				syncer.syncFromPgTable(pgSchemaTable, structureConn, copyConn)
+				syncer.syncerFullRefresh.SyncPgTable(pgSchemaTable, structureConn, copyConn)
 			}
 		}
 	}
@@ -171,76 +173,6 @@ func (syncer *Syncer) listPgSchemaTables(conn *pgx.Conn, schema string) []PgSche
 	return pgSchemaTables
 }
 
-func (syncer *Syncer) syncFromPgTable(pgSchemaTable PgSchemaTable, structureConn *pgx.Conn, copyConn *pgx.Conn) {
-	// Identify the batch size dynamically based on the table stats
-	rowCountPerBatch := syncer.calculateRowCountPerBatch(pgSchemaTable, structureConn)
-	LogDebug(syncer.config, "Row count per batch:", rowCountPerBatch)
-
-	// Create a capped buffer read and written in parallel
-	cappedBuffer := NewCappedBuffer(MAX_IN_MEMORY_BUFFER_SIZE, syncer.config)
-
-	var waitGroup sync.WaitGroup
-
-	// Copy from PG to cappedBuffer in a separate goroutine in parallel
-	waitGroup.Add(1)
-	go func() {
-		syncer.copyFromPgTable(pgSchemaTable, copyConn, cappedBuffer, &waitGroup)
-	}()
-
-	// Ping PG using structureConn in a separate goroutine in parallel to keep the connection alive
-	waitGroup.Add(1)
-	stopPingChannel := make(chan struct{})
-	go func() {
-		syncer.pingPg(structureConn, &stopPingChannel, &waitGroup)
-	}()
-
-	// Read the header to get the column names
-	csvReader := csv.NewReader(cappedBuffer)
-	csvHeader, err := csvReader.Read()
-	PanicIfError(err, syncer.config)
-
-	schemaTable := pgSchemaTable.ToIcebergSchemaTable()
-	pgSchemaColumns := syncer.pgTableSchemaColumns(structureConn, pgSchemaTable, csvHeader)
-	reachedEnd := false
-	totalRowCount := 0
-
-	// Write to Iceberg in a separate goroutine in parallel
-	LogInfo(syncer.config, "Writing to Iceberg:", pgSchemaTable.String()+"...")
-	syncer.icebergWriter.Write(schemaTable, pgSchemaColumns, func() [][]string {
-		if reachedEnd {
-			return [][]string{}
-		}
-
-		var rows [][]string
-		for {
-			row, err := csvReader.Read()
-
-			if err == io.EOF {
-				reachedEnd = true
-				break
-			}
-			if err != nil {
-				PanicIfError(err, syncer.config)
-			}
-
-			rows = append(rows, row)
-			if len(rows) >= rowCountPerBatch {
-				break
-			}
-		}
-
-		totalRowCount += len(rows)
-		LogDebug(syncer.config, "Writing", totalRowCount, "rows to Parquet...")
-		runtime.GC() // To reduce Parquet Go memory leakage
-
-		return rows
-	})
-
-	close(stopPingChannel) // Stop the pingPg goroutine
-	waitGroup.Wait()       // Wait for the Read goroutine to finish
-	LogInfo(syncer.config, "Finished writing to Iceberg:", pgSchemaTable.String())
-}
-
 func (syncer *Syncer) newConnection(ctx context.Context, databaseUrl string) *pgx.Conn {
 	conn, err := pgx.Connect(ctx, databaseUrl)
 	PanicIfError(err, syncer.config)
@@ -249,132 +181,6 @@ func (syncer *Syncer) newConnection(ctx context.Context, databaseUrl string) *pg
 	PanicIfError(err, syncer.config)
 
 	return conn
-}
-
-func (syncer *Syncer) pgTableSchemaColumns(conn *pgx.Conn, pgSchemaTable PgSchemaTable, csvHeader []string) []PgSchemaColumn {
-	if len(csvHeader) == 0 {
-		PanicIfError(errors.New("couldn't read data from "+pgSchemaTable.String()), syncer.config)
-	}
-
-	var pgSchemaColumns []PgSchemaColumn
-
-	rows, err := conn.Query(
-		context.Background(),
-		`SELECT
-			column_name,
-			data_type,
-			udt_name,
-			is_nullable,
-			ordinal_position,
-			COALESCE(character_maximum_length, 0),
-			COALESCE(numeric_precision, 0),
-			COALESCE(numeric_scale, 0),
-			COALESCE(datetime_precision, 0),
-			pg_namespace.nspname
-		FROM information_schema.columns
-		JOIN pg_type ON pg_type.typname = udt_name
-		JOIN pg_namespace ON pg_namespace.oid = pg_type.typnamespace
-		WHERE table_schema = $1 AND table_name = $2
-		ORDER BY array_position($3, column_name)`,
-		pgSchemaTable.Schema,
-		pgSchemaTable.Table,
-		csvHeader,
-	)
-	PanicIfError(err, syncer.config)
-	defer rows.Close()
-
-	for rows.Next() {
-		pgSchemaColumn := NewPgSchemaColumn(syncer.config)
-		err = rows.Scan(
-			&pgSchemaColumn.ColumnName,
-			&pgSchemaColumn.DataType,
-			&pgSchemaColumn.UdtName,
-			&pgSchemaColumn.IsNullable,
-			&pgSchemaColumn.OrdinalPosition,
-			&pgSchemaColumn.CharacterMaximumLength,
-			&pgSchemaColumn.NumericPrecision,
-			&pgSchemaColumn.NumericScale,
-			&pgSchemaColumn.DatetimePrecision,
-			&pgSchemaColumn.Namespace,
-		)
-		PanicIfError(err, syncer.config)
-		pgSchemaColumns = append(pgSchemaColumns, *pgSchemaColumn)
-	}
-
-	return pgSchemaColumns
-}
-
-func (syncer *Syncer) calculateRowCountPerBatch(pgSchemaTable PgSchemaTable, conn *pgx.Conn) int {
-	var tableSize int64
-	var rowCount int64
-
-	rows, err := conn.Query(
-		context.Background(),
-		`
-		SELECT
-			pg_total_relation_size(c.oid) AS table_size,
-			CASE
-				WHEN c.reltuples >= 0 THEN c.reltuples::bigint
-				ELSE (SELECT count(*) FROM `+pgSchemaTable.String()+`)
-			END AS row_count
-		FROM pg_class c
-		JOIN pg_namespace n ON n.oid = c.relnamespace
-		WHERE n.nspname = $1 AND c.relname = $2 AND c.relkind = 'r'`,
-		pgSchemaTable.Schema,
-		pgSchemaTable.Table,
-	)
-	PanicIfError(err, syncer.config)
-	defer rows.Close()
-
-	for rows.Next() {
-		err = rows.Scan(&tableSize, &rowCount)
-		PanicIfError(err, syncer.config)
-	}
-	LogDebug(syncer.config, "Table size:", tableSize, "Row count:", rowCount)
-
-	if tableSize == 0 || rowCount == 0 {
-		return 1
-	}
-
-	rowSize := tableSize / rowCount
-	rowCountPerBatch := int(MAX_PG_ROWS_BATCH_SIZE / rowSize)
-	if rowCountPerBatch == 0 {
-		return 1
-	}
-
-	return rowCountPerBatch
-}
-
-func (syncer *Syncer) copyFromPgTable(pgSchemaTable PgSchemaTable, copyConn *pgx.Conn, cappedBuffer *CappedBuffer, waitGroup *sync.WaitGroup) {
-	LogInfo(syncer.config, "Reading from PG:", pgSchemaTable.String()+"...")
-	result, err := copyConn.PgConn().CopyTo(
-		context.Background(),
-		cappedBuffer,
-		"COPY "+pgSchemaTable.String()+" TO STDOUT WITH CSV HEADER NULL '"+PG_NULL_STRING+"'",
-	)
-	PanicIfError(err, syncer.config)
-	LogInfo(syncer.config, "Copied", result.RowsAffected(), "row(s)")
-
-	cappedBuffer.Close()
-	waitGroup.Done()
-}
-
-func (syncer *Syncer) pingPg(conn *pgx.Conn, stopPingChannel *chan struct{}, waitGroup *sync.WaitGroup) {
-	ticker := time.NewTicker(PING_PG_INTERVAL_SECONDS * time.Second)
-
-	for {
-		select {
-		case <-*stopPingChannel:
-			LogDebug(syncer.config, "Stopping the ping...")
-			waitGroup.Done()
-			ticker.Stop()
-			return
-		case <-ticker.C:
-			LogDebug(syncer.config, "Pinging the database...")
-			_, err := conn.Exec(context.Background(), "SELECT 1")
-			PanicIfError(err, syncer.config)
-		}
-	}
 }
 
 func (syncer *Syncer) deleteOldIcebergSchemaTables(pgSchemaTables []PgSchemaTable) {
---
+++ src/syncer_full_refresh.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"io"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type SyncerFullRefresh struct {
+	config        *Config
+	icebergWriter *IcebergWriter
+}
+
+func NewSyncerFullRefresh(config *Config) *SyncerFullRefresh {
+	return &SyncerFullRefresh{
+		config:        config,
+		icebergWriter: NewIcebergWriter(config),
+	}
+}
+
+func (syncer *SyncerFullRefresh) SyncPgTable(pgSchemaTable PgSchemaTable, structureConn *pgx.Conn, copyConn *pgx.Conn) {
+	// Identify the batch size dynamically based on the table stats
+	rowCountPerBatch := syncer.calculateRowCountPerBatch(pgSchemaTable, structureConn)
+	LogDebug(syncer.config, "Row count per batch:", rowCountPerBatch)
+
+	// Create a capped buffer read and written in parallel
+	cappedBuffer := NewCappedBuffer(MAX_IN_MEMORY_BUFFER_SIZE, syncer.config)
+
+	var waitGroup sync.WaitGroup
+
+	// Copy from PG to cappedBuffer in a separate goroutine in parallel
+	waitGroup.Add(1)
+	go func() {
+		syncer.copyFromPgTable(pgSchemaTable, copyConn, cappedBuffer, &waitGroup)
+	}()
+
+	// Ping PG using structureConn in a separate goroutine in parallel to keep the connection alive
+	waitGroup.Add(1)
+	stopPingChannel := make(chan struct{})
+	go func() {
+		syncer.pingPg(structureConn, &stopPingChannel, &waitGroup)
+	}()
+
+	// Read the header to get the column names
+	csvReader := csv.NewReader(cappedBuffer)
+	csvHeader, err := csvReader.Read()
+	PanicIfError(err, syncer.config)
+
+	schemaTable := pgSchemaTable.ToIcebergSchemaTable()
+	pgSchemaColumns := syncer.pgTableSchemaColumns(structureConn, pgSchemaTable, csvHeader)
+	reachedEnd := false
+	totalRowCount := 0
+
+	// Write to Iceberg in a separate goroutine in parallel
+	LogInfo(syncer.config, "Writing to Iceberg:", pgSchemaTable.String()+"...")
+	syncer.icebergWriter.Write(schemaTable, pgSchemaColumns, func() [][]string {
+		if reachedEnd {
+			return [][]string{}
+		}
+
+		var rows [][]string
+		for {
+			row, err := csvReader.Read()
+
+			if err == io.EOF {
+				reachedEnd = true
+				break
+			}
+			if err != nil {
+				PanicIfError(err, syncer.config)
+			}
+
+			rows = append(rows, row)
+			if len(rows) >= rowCountPerBatch {
+				break
+			}
+		}
+
+		totalRowCount += len(rows)
+		LogDebug(syncer.config, "Writing", totalRowCount, "rows to Parquet...")
+		runtime.GC() // To reduce Parquet Go memory leakage
+
+		return rows
+	})
+
+	close(stopPingChannel) // Stop the pingPg goroutine
+	waitGroup.Wait()       // Wait for the Read goroutine to finish
+	LogInfo(syncer.config, "Finished writing to Iceberg:", pgSchemaTable.String())
+}
+
+func (syncer *SyncerFullRefresh) pgTableSchemaColumns(conn *pgx.Conn, pgSchemaTable PgSchemaTable, csvHeader []string) []PgSchemaColumn {
+	if len(csvHeader) == 0 {
+		PanicIfError(errors.New("couldn't read data from "+pgSchemaTable.String()), syncer.config)
+	}
+
+	var pgSchemaColumns []PgSchemaColumn
+
+	rows, err := conn.Query(
+		context.Background(),
+		`SELECT
+			column_name,
+			data_type,
+			udt_name,
+			is_nullable,
+			ordinal_position,
+			COALESCE(character_maximum_length, 0),
+			COALESCE(numeric_precision, 0),
+			COALESCE(numeric_scale, 0),
+			COALESCE(datetime_precision, 0),
+			pg_namespace.nspname
+		FROM information_schema.columns
+		JOIN pg_type ON pg_type.typname = udt_name
+		JOIN pg_namespace ON pg_namespace.oid = pg_type.typnamespace
+		WHERE table_schema = $1 AND table_name = $2
+		ORDER BY array_position($3, column_name)`,
+		pgSchemaTable.Schema,
+		pgSchemaTable.Table,
+		csvHeader,
+	)
+	PanicIfError(err, syncer.config)
+	defer rows.Close()
+
+	for rows.Next() {
+		pgSchemaColumn := NewPgSchemaColumn(syncer.config)
+		err = rows.Scan(
+			&pgSchemaColumn.ColumnName,
+			&pgSchemaColumn.DataType,
+			&pgSchemaColumn.UdtName,
+			&pgSchemaColumn.IsNullable,
+			&pgSchemaColumn.OrdinalPosition,
+			&pgSchemaColumn.CharacterMaximumLength,
+			&pgSchemaColumn.NumericPrecision,
+			&pgSchemaColumn.NumericScale,
+			&pgSchemaColumn.DatetimePrecision,
+			&pgSchemaColumn.Namespace,
+		)
+		PanicIfError(err, syncer.config)
+		pgSchemaColumns = append(pgSchemaColumns, *pgSchemaColumn)
+	}
+
+	return pgSchemaColumns
+}
+
+func (syncer *SyncerFullRefresh) calculateRowCountPerBatch(pgSchemaTable PgSchemaTable, conn *pgx.Conn) int {
+	var tableSize int64
+	var rowCount int64
+
+	rows, err := conn.Query(
+		context.Background(),
+		`
+		SELECT
+			pg_total_relation_size(c.oid) AS table_size,
+			CASE
+				WHEN c.reltuples >= 0 THEN c.reltuples::bigint
+				ELSE (SELECT count(*) FROM `+pgSchemaTable.String()+`)
+			END AS row_count
+		FROM pg_class c
+		JOIN pg_namespace n ON n.oid = c.relnamespace
+		WHERE n.nspname = $1 AND c.relname = $2 AND c.relkind = 'r'`,
+		pgSchemaTable.Schema,
+		pgSchemaTable.Table,
+	)
+	PanicIfError(err, syncer.config)
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&tableSize, &rowCount)
+		PanicIfError(err, syncer.config)
+	}
+	LogDebug(syncer.config, "Table size:", tableSize, "Row count:", rowCount)
+
+	if tableSize == 0 || rowCount == 0 {
+		return 1
+	}
+
+	rowSize := tableSize / rowCount
+	rowCountPerBatch := int(MAX_PG_ROWS_BATCH_SIZE / rowSize)
+	if rowCountPerBatch == 0 {
+		return 1
+	}
+
+	return rowCountPerBatch
+}
+
+func (syncer *SyncerFullRefresh) copyFromPgTable(pgSchemaTable PgSchemaTable, copyConn *pgx.Conn, cappedBuffer *CappedBuffer, waitGroup *sync.WaitGroup) {
+	LogInfo(syncer.config, "Reading from PG:", pgSchemaTable.String()+"...")
+	result, err := copyConn.PgConn().CopyTo(
+		context.Background(),
+		cappedBuffer,
+		"COPY "+pgSchemaTable.String()+" TO STDOUT WITH CSV HEADER NULL '"+PG_NULL_STRING+"'",
+	)
+	PanicIfError(err, syncer.config)
+	LogInfo(syncer.config, "Copied", result.RowsAffected(), "row(s)")
+
+	cappedBuffer.Close()
+	waitGroup.Done()
+}
+
+func (syncer *SyncerFullRefresh) pingPg(conn *pgx.Conn, stopPingChannel *chan struct{}, waitGroup *sync.WaitGroup) {
+	ticker := time.NewTicker(PING_PG_INTERVAL_SECONDS * time.Second)
+
+	for {
+		select {
+		case <-*stopPingChannel:
+			LogDebug(syncer.config, "Stopping the ping...")
+			waitGroup.Done()
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			LogDebug(syncer.config, "Pinging the database...")
+			_, err := conn.Exec(context.Background(), "SELECT 1")
+			PanicIfError(err, syncer.config)
+		}
+	}
+}
