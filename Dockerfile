911cf130ab10578f087b2e590e9325bc34b1edf5
exAspArk <exaspark@gmail.com>
2025-03-26 09:31:49 -0400

    Build docker images locally, automate publishing to GHCR

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ ARG PLATFORM
 ARG GOOS
 ARG GOARCH
 
-FROM --platform=$PLATFORM golang:1.23
+FROM --platform=$PLATFORM golang:1.23 AS builder
 
 WORKDIR /app
 
@@ -12,5 +12,12 @@ RUN go mod download
 COPY src/ .
 RUN CGO_ENABLED=1 GOOS=$GOOS GOARCH=$GOARCH go build -o /app/bemidb
 
-ENTRYPOINT [ "/app/bemidb" ]
-CMD [ "start" ]
+################################################################################
+
+FROM --platform=$PLATFORM debian:bookworm-slim
+
+WORKDIR /app
+
+COPY --from=builder /app/bemidb /app/bemidb
+
+ENTRYPOINT ["/app/bemidb"]
--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -2,19 +2,44 @@
 
 platforms=("linux/amd64" "linux/arm64")
 
+version=$(grep -E 'VERSION = "[^"]+"' src/config.go | sed -E 's/.*VERSION = "([^"]+)".*/\1/')
+if [ -z "$version" ]; then
+  echo "Error: Could not extract version from config.go"
+  exit 1
+fi
+
 for platform in "${platforms[@]}"
 do
   os="${platform%/*}"
   arch="${platform#*/}"
-  echo "Building bemidb for $os/$arch"
+  tag="ghcr.io/bemihq/bemidb:$version-$arch"
+
+  echo "Building bemidb version $version for $os/$arch"
 
   docker buildx build \
     --build-arg PLATFORM=$platform \
     --build-arg GOOS=$os \
     --build-arg GOARCH="$arch" \
-    -t bemidb-build:$os-$arch .
+    -t $tag .
 
-  docker create --name temp-container bemidb-build:$os-$arch
+  docker create --name temp-container $tag
   docker cp temp-container:/app/bemidb ./build/bemidb-$os-$arch
   docker rm temp-container
 done
+
+echo "Pushing bemidb version $version to ghcr.io"
+
+docker push ghcr.io/bemihq/bemidb:$version-amd64
+docker push ghcr.io/bemihq/bemidb:$version-arm64
+
+docker manifest create ghcr.io/bemihq/bemidb:$version \
+  ghcr.io/bemihq/bemidb:$version-amd64 \
+  ghcr.io/bemihq/bemidb:$version-arm64
+
+docker manifest annotate ghcr.io/bemihq/bemidb:$version ghcr.io/bemihq/bemidb:$version-amd64 --arch amd64
+docker manifest annotate ghcr.io/bemihq/bemidb:$version ghcr.io/bemihq/bemidb:$version-arm64 --arch arm64
+
+docker manifest push ghcr.io/bemihq/bemidb:$version
+
+docker tag ghcr.io/bemihq/bemidb:$version ghcr.io/bemihq/bemidb:latest
+docker push ghcr.io/bemihq/bemidb:latest
